0e410c4f9bdebb820d3c07bf0c827fb10d81dc68
Sam Beckett <sbeck14@me.com>
2021-10-19 11:01:42 -0600

    add actions:create stub

--- plugins/actions/index.js
+++ plugins/actions/index.js
@@ -73,6 +73,35 @@ async function listActions(appkit, args) {
   }
 }
 
+/**
+ * createActions - Create a new action on an app
+ */
+async function createActions(appkit, args) {
+  // Check to make sure we have passed in required arguments
+  try {
+    assert.ok(args.app && args.app !== '', 'An application name was not provided.');
+    assert.ok(args.size && args.size !== '', 'An action size was not provided.');
+    assert.ok(args.name && args.name !== '', 'A name was not provided.');
+  } catch (err) {
+    appkit.terminal.error(err);
+    process.exit(1);
+  }
+
+  // All possible arguments (some of these are optional)
+  const {
+    app, description, size, command, image, env, name,
+  } = args;
+
+  /*
+    Code goes here
+    Helpful functions:
+      appkit.api.post(body, url); -- POST the given body to the given URL
+      console.log(str); -- Output a message to the console
+
+    Code should create a JSON object with the payload and POST it to the actions endpoint
+  */
+}
+
 module.exports = {
   init(appkit) {
     const require_app_option = {
@@ -84,8 +113,48 @@ module.exports = {
       },
     };
 
+    const create_action_option = {
+      app: {
+        alias: 'a',
+        demand: true,
+        string: true,
+        description: 'The app to act on',
+      },
+      description: {
+        alias: 'd',
+        demand: false,
+        string: true,
+        description: 'An optional description of the action',
+      },
+      size: {
+        alias: 's',
+        demand: true,
+        string: true,
+        description: 'The dyno size to use for the action\'s formation',
+      },
+      command: {
+        alias: 'c',
+        demand: false,
+        string: true,
+        description: 'An optional command to use for the action\'s image',
+      },
+      image: {
+        alias: 'i',
+        demand: false,
+        string: true,
+        description: 'An optional image to use for the action instead of the app\'s image',
+      },
+      env: {
+        alias: 'e',
+        demand: false,
+        string: true,
+        description: 'One or more key-value pairs (KEY=VALUE) to use as additional environment variables',
+      },
+    };
+
     appkit.args
-      .command('actions', 'List available actions on an app', require_app_option, listActions.bind(null, appkit));
+      .command('actions', 'List available actions on an app', require_app_option, listActions.bind(null, appkit))
+      .command('actions:create NAME', 'Create action on an app', create_action_option, createActions.bind(null, appkit));
   },
   update() {
     // do nothing.
